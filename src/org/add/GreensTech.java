6dd2fd8960cfef95bf84bc1d8074dcf081e8e07a
Bhuvi-0512 <bhuvikesavan5@gmail.com>
2022-01-28 11:57:15 +0530

    Project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,42 @@
+package org.add;
+
+public class GreensTech {
+	
+	public static void main(String[] args) {
+		
+		GreensTech GT= new GreensTech ();
+		
+				GT.greensOmr();
+				GT.greensAdayar();
+				GT.greensTambaram();
+				GT.greensVelachery();
+				GT.greensAnnaNagar();
+	}
+	
+	private void greensOmr() {
+		
+		System.out.println("Greens OMR");
+		
+	}
+	
+	private void greensAdayar() {
+		
+		System.out.println("Greens Adayar");
+	}
+	
+	private void greensTambaram() {
+	
+		System.out.println("Greens Tambaram");
+	}
+	
+	private void greensVelachery() {
+		
+		System.out.println("Greens Velachery");
+	}
+	
+	private void greensAnnaNagar() { 
+		
+		System.out.println("Greens Anna Nagar");
+	}
+
+}
